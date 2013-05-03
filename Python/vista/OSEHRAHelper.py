d6fba6f1c5f3155951339a8602db79d6605ea884
Andal FeQuiere <andal.fequiere@us.pwc.com>
2013-05-02 22:40:51 -0400

    updated code for GTM environment

--- Python/vista/OSEHRAHelper.py
+++ Python/vista/OSEHRAHelper.py
@@ -128,7 +128,7 @@ class ConnectWinCache(ConnectMUMPS):
         logging.debug(rbuf)
         return 1
 
-  def wait_re(self, command, timeout=30):
+  def wait_re(self, command, timeout=20):
     if command is PROMPT:
       command = self.prompt
     output = self.connection.expect(command, timeout)
@@ -219,9 +219,19 @@ class ConnectLinuxCache(ConnectMUMPS):
     else:
         return 1
 
-  def wait_re(self, command, timeout=15):
-    if not timeout: timeout = -1
-    self.connection.expect(command, timeout)
+  def wait_re(self, command, timeout=20):
+    if command is PROMPT:
+      command = self.prompt
+    output = self.connection.expect(command, timeout)
+    self.match = output[1]
+    self.before = output[2]
+    if output[0] == -1 and output[1] == None:
+        logging.debug('Timeout - ERROR: expected: ' + str(command))
+        raise TestHelper.TestError('Timeout - ERROR: expected: ' + str(command))
+    if output[2]:
+      self.log.write(output[2])
+      self.log.flush()
+      return output
 
   def multiwait(self, options, tout=15):
     if isinstance(options, list):
@@ -302,9 +312,19 @@ class ConnectLinuxGTM(ConnectMUMPS):
     else:
         return 1
 
-  def wait_re(self, command, timeout=None):
-    if not timeout: timeout = -1
-    self.connection.expect(command, timeout)
+  def wait_re(self, command, timeout=20):
+    if command is PROMPT:
+      command = self.prompt
+    output = self.connection.expect(command, timeout)
+    self.match = output[1]
+    self.before = output[2]
+    if output[0] == -1 and output[1] == None:
+        logging.debug('Timeout - ERROR: expected: ' + str(command))
+        raise TestHelper.TestError('Timeout - ERROR: expected: ' + str(command))
+    if output[2]:
+      self.log.write(output[2])
+      self.log.flush()
+      return output
 
   def multiwait(self, options, tout=15):
     if isinstance(options, list):
